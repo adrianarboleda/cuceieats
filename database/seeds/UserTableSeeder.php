593991d7badda4f9d64b11d4ae92f30fd20b8f8c
adrianarboleda <adrian.arboleda@alumnos.udg.mx>
2022-04-05 00:07:40 -0500

    feature - seeds - seeder del user Admin

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::where('email', 'adrianarboledadavis@gmail.com')->first();
+
+        if(!$user){
+            User::create([
+                'name' => 'Admin',
+                'email' => 'adrianarboledadavis@gmail.com',
+                'password' => Hash::make('123$%&'),
+                'role' => 'admin'
+            ]);
+        }
+    }
+}
