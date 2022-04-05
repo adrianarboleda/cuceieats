ff52d4e43d72292bd5d70da868101b49b113d7c1
adrianarboleda <adrian.arboleda@alumnos.udg.mx>
2022-04-05 00:38:48 -0500

    fix - routes web.php - nombramiento a ruta productos

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('/producto', function () {
     return view('producto.index');
 });
 
-Route::resource('/productos', 'ProductoController')->middleware('auth');
+Route::resource('/productos', 'ProductoController')->name('index','productos')->middleware('auth');
 
 Route::get('/home', 'HomeController@index')->name('home');
